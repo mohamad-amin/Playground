f493cfc33d7153d72e7151fcc9349d10ae9a62c4
mohamad-amin <mohamadamin@aut.ac.ir>
2017-09-16 03:55:43 +0430

    Added StringSplosion problem's answer

---
+++ Java/TechDev/Foundational/src/main/java/com/mohamadamin/techdev/foundational/StringSplosion.java
@@ -0,0 +1,21 @@
+package com.mohamadamin.techdev.foundational;
+
+/**
+ * Created by mohamadamin (torpedo.mohammadi@gmail.com) on 9/16/17.
+ */
+
+public class StringSplosion {
+
+    public static void main(String[] args) {
+        System.out.println(new StringSplosion().stringSplosion("Code"));
+    }
+
+    public String stringSplosion(String str) {
+        StringBuilder sploded = new StringBuilder();
+        for (int i=0; i <= str.length(); i++) {
+            sploded.append(str.substring(0, i));
+        }
+        return sploded.toString();
+    }
+
+}
